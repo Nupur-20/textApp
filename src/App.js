96c940976290767e4635b9c0b4db894f19cf0cd4
Nupur-20 <ntiffco@gmail.com>
2023-07-23 22:12:17 +0530

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-//import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 
 
@@ -28,9 +33,9 @@ function App() {
   const toggleMode = ()=>{
     if (mode==='light'){
       setMode('dark');
-      document.body.style.backgroundColor='#22173d';
+      document.body.style.backgroundColor='rgb(26 37 68)';
       showAlert("darkmode has been enabled","success");
-      document.title='Textutils-Darkmode';
+      //document.title='Textutils-Darkmode';
     //  setInterval(() => {
       //  document.title='Textutils is amazing';
     //},2000);
@@ -54,10 +59,16 @@ function App() {
 <Alert alert={alert}/>
 <div className="container my-3">
 <TextForm showAlert={showAlert} heading ="Enter text to analyze" mode={mode}/> 
-
- </div> 
+<About mode={mode}/> 
+{/* <Routes>
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>}/>
+          <Route exact path="/about" element={<About/>}/>
+      </Routes> */}
+      
+      {/* </Router> */} 
+      </div>
  </>
   );
 }
-
+ 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,49 +1,63 @@
+// rfc
+// useState is a hook which hepls to create states
 import React,{useState} from 'react'
+
 export default function TextForm(props) {
+    // text="hello there";
+    // this type of syntax is not allowed in react to change any variables value
+    // we use this here ;-below bdo
+    const [text,setText]=useState('');
+    // setText("hello there");// correct way to change the state
+
     const handleUpClick=()=>{
-        console.log("uppercase is clicked"+text );
         let newText=text.toUpperCase();
         setText(newText)
-        //props.showAlert("converted to uppercase","success");
+        props.showAlert("Converted to uppercase!","success");
+      }
+      const handleLowClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!","success");
+      }
+      const handleClearClick=()=>{
+        let newText='';
+        setText(newText)
+        props.showAlert("Text cleared!","success");
+      }
+     const handleInverseClick=()=>{
+        let temp=text.split(" ");
+        let reverseTemp=temp.reverse();
+        let newText=reverseTemp.join(" ");
+        setText(newText)
+        props.showAlert("Inverse text generated!","success");
+      }
+      const handleCopyClick=()=>{
+        navigator.clipboard.writeText(text);
+        props.showAlert("Text copied to clipboard!","success");
     }
-    const handleLoClick=()=>{
-      console.log("uppercase is clicked"+text );
-      let newText=text.toLowerCase();
-      setText(newText)
-      //props.showAlert("converted to lowercase","success");
-  }
-  const handleClearClick=()=>{
-    console.log("CLEARED"+text );
-    let newText="";
-    setText(newText)
-}
     const handleOnChange=(event)=>{
-        console.log("on change");
-        setText(event.target.value);
-        
+        setText(event.target.value)
     }
-const [text, setText] = useState('');
-return (
-  <>
-  <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+  return (
+    <>
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
     <h1>{props.heading}</h1>
     <div className="mb-3">
-    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#22173d'}} id="mybox" rows="8"></textarea>
-  </div>
-  <button className="btn btn-primary mx-2" onClick={handleUpClick}>convert to uppercase</button>
-  <button className="btn btn-primary mx-2" onClick={handleLoClick}>convert to lowercase</button>
-  <button className="btn btn-primary MX3" onClick={handleClearClick}>clear</button>
- </div>
-    <div className="container my-3"style={{color: props.mode==='dark'?'white':'#22173d'}}>
-    <h3>your text summary </h3>
-    <p>{text.split(" ").length} words and {text.length} charecters</p>
-    <p>{0.008*text.split(" ").length} minutes read </p>
-    <h2>Preview</h2>
-    <p>{text.length>0?text:"enter something in the textbox above to preview it"}</p>
+     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+    </div>
+    <button className="btn btn-primary mx-2 my-3" onClick={handleUpClick}>convert to upper case</button>
+    <button className="btn btn-primary mx-2 my-3" onClick={handleLowClick}>convert to lowercase case</button>
+    <button className="btn btn-primary mx-2 my-3" onClick={handleClearClick}>Clear text</button>
+    <button className="btn btn-primary mx-2 my-3" onClick={handleInverseClick}>Inverse text</button>
+    <button className="btn btn-primary mx-2 my-3" onClick={handleCopyClick}>Copy text</button>
+    </div>
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1>Your text summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008* text.split(" ").length} Minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter somthing in the textbox to preview it here"}</p>
     </div>
     </>
   )
 }
- 
-
- 
